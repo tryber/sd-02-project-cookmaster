9aa044e8e88fc1aa523d6f5b8537bd71fc437edc
phtf92 <phtf92@gmail.com>
2020-06-23 18:28:27 -0300

    CC first requisite

--- models/rootModel.js
+++ models/rootModel.js
@@ -2,14 +2,14 @@ const connection = require('./connection');
 
 const findAllRecipes = async () => {
   try {
-    const db = await connection()
+    const db = await connection();
     const results = await db.getTable('Recipes').select(['recipe_name', 'creator_id']).execute();
     const recipes = await results.fetchAll();
     return recipes;
   } catch (e) {
     console.log(e);
-  };
-}
+  }
+};
 
 module.exports = {
   findAllRecipes,
