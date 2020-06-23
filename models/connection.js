3e0d2f17a2a4ae8abfb96ea16f1cfb7145b869ad
phtf92 <phtf92@gmail.com>
2020-06-23 16:12:32 -0300

    CC connections

--- models/connection.js
+++ models/connection.js
@@ -3,21 +3,20 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
-const connection = () => {
-  return mysqlx.getSession({
+const connection = () => (
+  mysqlx.getSession({
     user: 'root',
     password: process.env.DATABASE_PASSWORD,
     host: 'localhost',
     port: 33060,
     schema: 'cook_master',
   })
-    .then((session) => {
+    .then(session => {
       return session.getSchema('cook_master');
     })
     .catch((err) => {
       console.error(err);
       process.exit(1);
-    });
-};
+    }));
 
 module.exports = connection;
