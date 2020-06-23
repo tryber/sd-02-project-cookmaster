4289640d579ca141c2ef532d062afe95278145f6
douglas-he <doughrqe@gmail.com>
2020-06-23 18:51:12 -0300

    REQUISITO 1 GLORIA

---
+++ controllers/recipeController.js
@@ -0,0 +1,9 @@
+const getRecipes = require('../models/getRecipes');
+
+const receiveRecipes = async (req, res) => {
+  const response = await getRecipes();
+  const recipes = await response.fetchAll();
+  res.render('home', { recipes, token: req.headers.cookie});
+};
+
+module.exports = receiveRecipes;
