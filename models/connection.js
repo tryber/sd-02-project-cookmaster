20e7bd145b3ecc0f0f65d8720e26064af99bf95f
phtf92 <phtf92@gmail.com>
2020-06-23 15:44:37 -0300

    Creating DB connection

---
+++ models/connection.js
@@ -0,0 +1,20 @@
+const mysqlx = require('@mysql/xdevapi');
+
+const connection = () => {
+  return mysqlx.getSession({
+    user: 'root',
+    password: '******',
+    host: 'localhost',
+    port: 33060,
+    schema: 'cook_master',
+  })
+    .then((session) => {
+      return session.getSchema('cook_master');
+    })
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
+};
+
+module.exports = connection;
