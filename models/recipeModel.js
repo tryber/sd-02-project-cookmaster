5f3c3ca46cd89ea772b7c559a2aac990d19eca24
guicgs <etohlif@protonmail.com>
2020-06-23 21:58:49 -0300

    CC

--- models/recipeModel.js
+++ models/recipeModel.js
@@ -3,7 +3,7 @@ const useSession = require('./useSession');
 const getAllRecipes = async () =>
   useSession()
     .then((session) => session
-      .sql(`SELECT 
+      .sql(`SELECT
             r.id,
             r.recipe_name AS recipeName,
             CONCAT(u.first_name, ' ', u.last_name) AS userName
@@ -18,14 +18,14 @@ const getAllRecipes = async () =>
     .then((recipes) =>
       recipes.map(([recipeId, recipeName, userName]) => ({ recipeId, recipeName, userName })));
 
-const getSingleRecipe = async (idRecipe) => 
+const getSingleRecipe = async (idRecipe) =>
   useSession()
     .then((session) => session
-      .sql(`SELECT 
-            r.id, 
-            r.recipe_name, 
-            r.ingredients, 
-            r.instructions, 
+      .sql(`SELECT
+            r.id,
+            r.recipe_name,
+            r.ingredients,
+            r.instructions,
             (SELECT CONCAT(u.first_name, ' ', u.last_name) FROM users u WHERE r.user_id = u.id)
             FROM recipes r
             WHERE r.id = ${idRecipe};`)
