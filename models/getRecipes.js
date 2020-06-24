647f6df150daaa593a7848c3fc37e436a5cb82d1
douglas-he <doughrqe@gmail.com>
2020-06-23 22:06:40 -0300

    CC

--- models/getRecipes.js
+++ models/getRecipes.js
@@ -4,7 +4,7 @@ const getRecipes = async () =>
   requestSession()
     .then((session) =>
       session.sql('SELECT r.recipe_name, u.first_name, r.id FROM cookie_master.Users AS u INNER JOIN cookie_master.Recipes AS r ON r.creator_id = u.id;').execute())
-    .then((results) => results)
+    .then((results) => results);
 
 
 module.exports = getRecipes;
