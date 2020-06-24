55356add9d1976fe5a1fdfac333ecc785714d0cf
douglas-he <doughrqe@gmail.com>
2020-06-24 18:06:46 -0300

    CC

--- controllers/postNewRecipe.js
+++ controllers/postNewRecipe.js
@@ -5,6 +5,6 @@ const postNewRecipe = async (req, res) => {
   const validate = await postRecipe({ ...req.body, id });
   if (typeof (validate) === 'string') return res.render('admin/formRecipe', { message: validate });
   return res.redirect('/');
-}
+};
 
 module.exports = postNewRecipe;
