bd59f4da63d40a73cc5b6558c2edb5ef3c9cd563
douglas-he <doughrqe@gmail.com>
2020-06-23 22:07:30 -0300

    CC

--- controllers/recipeDetails.js
+++ controllers/recipeDetails.js
@@ -1,7 +1,7 @@
 const oneRecipe = require('../models/oneRecipe');
 
 const recipeDetails = async (req, res) => {
-  const recipe = await oneRecipe(req.params.id)
+  const recipe = await oneRecipe(req.params.id);
   const { id } = req.user || false;
   res.render('details', { recipe, token: id });
 };
