05d423d7fd961afd87d0b25d208ae974c9d98ea2
douglas-he <doughrqe@gmail.com>
2020-06-25 16:50:43 -0300

    CC

--- controllers/recipeDelete.js
+++ controllers/recipeDelete.js
@@ -4,6 +4,6 @@ const recipeDelete = async (req, res) => {
   const recipe = await oneRecipe(req.params.id);
   if (req.user.id !== recipe[4]) return res.redirect(`/recipes/${recipe[0]}`);
   return res.render('admin/formDelete', { id: recipe[0] });
-}
+};
 
 module.exports = recipeDelete;
