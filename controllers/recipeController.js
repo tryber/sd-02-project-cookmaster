2b94970fd7bbce6b4e5458a743c32e3ec834f255
eddyeboy27 <lipe0503@gmail.com>
2020-06-26 15:40:29 -0300

    Inserido botoes de login, e voltar

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -3,12 +3,13 @@ const recipeModel = require('../models/recipeModel');
 const findRecipes = async (req, res) => {
   const recipes = await recipeModel.getRecipesFromDataBase();
   if (!recipes) return res.render('./404/notFound');
-  res.render('./recipes/recipeView', { recipes, logged: req.user || 'empty' });
+  res.render('./recipes/recipeView', { recipes, logged: req.user });
 };
 
 const findRecipeDetail = async (req, res) => {
   const { id } = req.params;
   const recipe = await recipeModel.getRecipeDetails(id);
+  console.log(req)
   res.render('./recipes/recipeDetailsView', { recipe, logged: req.user || 'empty' });
 }
 module.exports = {
