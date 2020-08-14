4901f1233a8a13b419d9960c17f6b016e44d2638
Mateus Talles <mateustalles@gmail.com>
2020-08-14 20:05:14 -0300

    code climate again

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -1,17 +1,17 @@
-const recipesModel = require('../models/recipesModel')
+const recipesModel = require('../models/recipesModel');
 
 const recipesLandingPage = async (_req, res) => {
   const recipesData = await recipesModel.readRecipes();
   return res.render('home', { recipesData });
-}
+};
 
 const recipeDetails = async (req, res) => {
   const recipeID = req.params.id;
-  const  { id, name, description, ingredients } = await recipesModel.readRecipes(Number(recipeID));
+  const { id, name, description, ingredients } = await recipesModel.readRecipes(Number(recipeID));
   return res.render('recipeDetails', { user: req.user, recipe: { id, name, description, ingredients } });
-}
+};
 
 module.exports = {
   recipesLandingPage,
-  recipeDetails
+  recipeDetails,
 };
